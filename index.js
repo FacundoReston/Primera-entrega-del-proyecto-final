48757fea98bd9923b8964e123a6d913c7c80dc98
FacundoReston <107761946+FacundoReston@users.noreply.github.com>
2022-09-24 23:59:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const suma = (a, b) => a + b;                   // Funciones flecha //   
+const suma = (a, b) => a + b;    /* Funciones flecha*/   
 const resta = (a, b) => a - b;   
-const iva = x => x * 0.21;        
-const venta = y => y * 3;       
+const iva = x => x * 0.21;       
+const venta = y => y * 3;        
 const descuento = z => z * 0.10;
-let precioCosto = prompt("Ingrese el precio del articulo");
+let nombre = prompt("Ingrese el nombre del articulo")
+let precioCosto = parseInt(prompt("Ingrese el precio del articulo"));
 let precioVenta = parseInt(suma(venta(precioCosto), iva(precioCosto)));
+// Al comprar articulos para vender en mi negocio, voy a multiplicar su valor por 3 y sumarle el iva //
 
-
-    // Al comprar articulos para vender en mi negocio, voy a multiplicar su valor por 3 y sumarle el iva //
-
+const miArreglo = ["giudicatti", "gitano", "usual", "rusty"];
 
 if (precioCosto > 5000) {
     alert("El precio excede el presupuesto del negocio");
@@ -23,5 +23,16 @@ if (precioCosto > 5000) {
         alert(`El precio con descuento es ${nuevoPrecio}`);
         alert("Ubicación del articulo: Vidriera");
     }
-}   // Condicional para saber si puedo comprar ciertos articulos, para darles una ubicacion en mi local y para hacer un descuento si superan los $8000 //
+}  // Condicional para saber si puedo comprar ciertos articulos, para darles una ubicacion en mi local y para hacer un descuento si superan los $8000 //
+
+if (nombre !== "") {
+    miArreglo.push(nombre);
+    alert(miArreglo);
+} else {
+    alert("No se ingresó el nombre");
+}
+
+
+
+
 
